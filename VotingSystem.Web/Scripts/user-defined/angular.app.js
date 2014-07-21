e450b66d0ae6e67eae2b11eb493f403ac2339a2f
Nicolai8 <bolotin.nicolai@gmail.com>
2014-07-21 16:04:34 +0300

    + admin votings page to angular js

--- VotingSystem.Web/Scripts/user-defined/angular.app.js
+++ VotingSystem.Web/Scripts/user-defined/angular.app.js
@@ -1,11 +1,14 @@
 ﻿define(["angular", "controllers/angular.controller.layout", "controllers/angular.controller.main"
+	, "controllers/angular.controller.adminvotings"
 	, "directives/angular.directive.paginator", "directives/angular.directive.validateform"
 	, "directives/angular.directive.focusout"
 	, "angular.route"],
-	function (angular, layoutController, mainController, paginatorDirective, validateFormDirective, focusOutDirective) {
+	function (angular, layoutController, mainController, adminVotingsController,
+		paginatorDirective, validateFormDirective, focusOutDirective) {
 		var votingsystemControllers = angular.module('votingsystemControllers', []);
 		layoutController(votingsystemControllers);
 		mainController(votingsystemControllers);
+		adminVotingsController(votingsystemControllers);
 
 		var votingSystem = angular.module('votingSystem', [
 			'ngRoute',
@@ -19,6 +22,9 @@
 			}).when('/mainpage/:pageNumber/:searchQuery', {
 				controller: 'MainController',
 				templateUrl: "static/main.html",
+			}).when('/adminvotingspage/:pageNumber', {
+				controller: 'AdminVotingsController',
+				templateUrl: "static/adminvotings.html",
 			}).
 			otherwise({
 				redirectTo: '/mainpage/1'
---
+++ VotingSystem.Web/Scripts/user-defined/controllers/angular.controller.adminvotings.js
@@ -0,0 +1,57 @@
+﻿define(["angular", "Urls", "constants", "toastr", "bootpag", "angular.route"],
+	function (angular, Urls, constants, toastr) {
+		return function (controllersModule) {
+			controllersModule
+				.controller('AdminVotingsController', function ($scope, $http, $route, $routeParams, $location) {
+					$scope.page = $routeParams.pageNumber;
+					$scope.pageName = "adminvotingspage";
+					$scope.total = 1;
+					$scope.constants = constants;
+					$scope.$location = $location;
+					$scope.$route = $route;
+
+					$http.get(Urls.Votings + "/AdminVotings/" + $routeParams.pageNumber).success(function (data) {
+						$scope.votings = data;
+						$http.get(Urls.AdminPage.GetTotal).success(function (total) {
+							$scope.total = total;
+						});
+					});
+
+					$scope.setThemeStatus = function (voting, status) {
+						$http.put(Urls.Votings + "/" + voting.VotingId, { "Status": status })
+							.success(function () {
+								toastr.success(constants("votingStatusChangedMessage"));
+								voting.Status = status;
+							}).error(function () {
+								toastr.error(constants("errorOccurredDuringSavingProcessMessage"));
+							});
+					};
+
+					$scope.removeTheme = function (voting) {
+						$http.delete(Urls.Votings + "/" + voting.VotingId)
+							.success(function () {
+								$scope.votings.splice($scope.votings.indexOf(voting), 1);
+								toastr.success(constants("votingDeletedMessage"));
+								$scope.render();
+							}).error(function () {
+								toastr.error(constants("errorOccurredDuringDeletingProcessMessage"));
+							});
+					};
+
+					$scope.render = function () {
+						var pageNumber = $scope.page;
+						if (pageNumber != 1) {
+							if (votings.length == 0) {
+								pageNumber -= 1;
+							}
+						}
+						if (pageNumber != $scope.page) {
+							$scope.$location.path("/adminvotingspage/" + pageNumber);
+						} else {
+							$route.reload();
+						}
+					};
+				});
+		};
+	});
+

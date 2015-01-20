ff3e674fdbf972cbde14ca62eeca74de357aaa10
pryabov <prpbox@gmail.com>
2015-01-20 14:53:16 +0300

    Added missing files

---
+++ VotingSystem.Common/Filters/FilterGeneric.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace VotingSystem.Common.Filters
+{
+	public class Filter<T> : Filter
+	{
+		public Func<IQueryable<T>, IOrderedQueryable<T>> OrderBy { get; set; }
+
+		public Filter()
+		{
+			OrderBy = null;
+		}
+
+		public Filter(Func<IQueryable<T>, IOrderedQueryable<T>> orderBy, int page, int pageSize)
+			: base(page, pageSize)
+		{
+			OrderBy = orderBy;
+		}
+	}
+}
